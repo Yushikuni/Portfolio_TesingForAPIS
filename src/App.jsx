35ad990c0e9c596dcf7983a3205ce99fb7941ac4
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2024-07-21 18:58:07 +0200

    fixing is still in progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@
 //import viteLogo from '/vite.svg'
 import './App.css';
 
-//import { Container, Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Profile from './components/Profile/Profile';
-//import Header from './components/Header/Header';
-//import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Portfolio from './pages/Portfolio/Portfolio';
-/*import Resume from './pages/Resume/Resume';*/
+import Resume from './pages/Resume/Resume';
 
 //import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
@@ -18,50 +18,48 @@ import Portfolio from './pages/Portfolio/Portfolio';
 function App() {
     //const [count, setCount] = useState(0)
     return (
-        //<>
-        //    Tady ma byt:<br/>
-        //    container<br/>
-        //    grid<br/>
-
-        //    <Profile />
-
-        //    <Header />
-
-        //    <Resume />
-
-        //    <Portfolio />
-
-        //    <Footer />
-        //    <br /><br /><br /><br /><br /><br /><br />
-        //    TODO:<br />
-        //    Co vsechno ma issue s Vite + REACTJS?<br />
-        //    <br />1. HEADER!!
-        //    <br />2. UTF-8 ale to neva, web je v AJ :)
-        //    <br />3. Routerovani
-        //    <br />4.
-
-        //</>
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">
-                                Profile
-                            </Link>
-                            <Link to="/portfolio">
-                                Portfolio
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/" component={Profile}/>
-                    <Route path="/portfolio" component={Portfolio}/>
-                    {/*Pridani dalsih stranek*/}
-                </Switch>
-            </div>
-        </Router>
+        <Container className={'top_60'}>
+            <Grid container spacing={7}>
+                <Grid item xs={12} sm={12} md={4} lg={3}>
+                    <Profile />
+                </Grid>
+                <Grid item xs>
+                    <Router>
+                        <Header />
+                        <div className='main-content container_shadow'>
+                            <Switch>
+                                <Route path="/portfolio"> <Portfolio /> </Route>
+                                <Route path='/'> <Resume /> </Route>
+                            </Switch>
+                        </div>
+                    </Router>
+                    <Footer />
+                </Grid>
+            </Grid>
+        </Container>        
+        
+           
+        //<Router>
+        //    <div>
+        //        <nav>
+        //            <ul>
+        //                <li>
+        //                    <Link to="/">
+        //                        Profile
+        //                    </Link>
+        //                    <Link to="/portfolio">
+        //                        Portfolio
+        //                    </Link>
+        //                </li>
+        //            </ul>
+        //        </nav>
+        //        <Switch>
+        //            <Route path="/" component={Profile}/>
+        //            <Route path="/portfolio" component={Portfolio}/>
+        //            {/*Pridani dalsih stranek*/}
+        //        </Switch>
+        //    </div>
+        //</Router>
 
         
     //<Sidebar>
