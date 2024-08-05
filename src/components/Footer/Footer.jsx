4aac2b4e186ee043988be0bd3cf5fd6b9df17bf5
Kvetuse Husakova <kvetuse.husakova@unicodesys.cz>
2024-08-05 15:05:31 +0200

    update a footer delete a note for inspired by also update a line in a element and delete another comments

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,15 +28,9 @@ const Footer = ()=> {
                 <Typography className="footer_copyrights">
                     Designed and developed by © <a href="/" target='_blank'>Květuše Husáková</a>
                     <br/>
-                    Inspired by <a href="https://dev.epicgames.com/community/unreal-engine" target="_blank" rel="noreferrer">Dev.EpicGames.com</a>.<br/>
+                    Inspired by <a href="https://dev.epicgames.com/community/" target="_blank" rel="noreferrer" >EpicGames</a>.<br/>
                  </Typography>
             </div>
-            {/*<div className="footer_right">
-                <Typography className="footer_copyrights">
-                    Designed and developed by © <a href="/" target='_blank'>Květuše Husáková</a>
-                    <br/>
-                    Inspired by <a href="https://dev.epicgames.com/community/" target="_blank" rel="noreferrer" >EpicGames</a>.<br/>
-            </div-->*/}
         </div>
     );
 };
